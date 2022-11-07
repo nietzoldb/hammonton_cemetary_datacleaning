4039baf954cc055b5192b1df42e04f123de20097
nietzolb <nietzolb@go.stockton.edu>
2022-11-06 23:33:01 -0500

    Additional progress having difficulty data types

--- Halloween.py
+++ Halloween.py
@@ -30,9 +30,6 @@ num_files = len(fileNames)
 i = 1
 for f in fileNames:
     df = pd.read_csv("C:/Users/bniet/github-classroom/hammonton_cemetary_datacleaning/Data/"+f,sep=',')
-    print('This is the column names for the df_%s:' %i)
-    for col in df.columns:
-     print(col)
     globals()[f"df_{i}"] = df
     i +=1
 
@@ -72,96 +69,215 @@ df_3_NameClean = df_3[['FirstName','LastName','DOB','DOD']]
 df_4_NameClean = df_4[['FirstName','LastName','DOB','DOD']]
 df_concat = pd.concat([df_1_NameClean,df_2_NameClean,df_3_NameClean,df_4_NameClean], axis=0, ignore_index=True)
 df_concat.info()
-#print(df_1_NameClean)
-#print(df_2_NameClean)
-#print(df_3_NameClean)
-#print(df_4_NameClean)
-print(df_concat)
 
 '''This loop parses the DOB and DOD columns in df_concat keeping only the year for each.   
 '''
+k_max=0
+n_max=0
+l_max=0
+k3_count=0
+k2_count=0
+n3_count=0
+n2_count=0 
+l3_count=0
+l2_count=0
+all1_count=0
+j=0
+m = df_concat.shape[0]
+while j < m:
+    
+        
+    k=len(str(df_concat.iloc[j,2]).split('/'))
+    n=len(str(df_concat.iloc[j,2]).split('-'))
+    l=len(str(df_concat.iloc[j,2]).split(' '))
+    p = max(k,n,l)
+    if k > k_max:
+        k_max=k
+
+    if n > n_max:
+        n_max=n
+        
+    if l > l_max:
+        l_max=l
+ 
+    if j == 115:
+        print(k,l,n)
+    
+    if k == max(k,n,l):
+        if k >= 3:
+            print(k,l,n)
+            df_concat['DOB'][j] = str(df_concat['DOB'][j]).split('/')[k-1]
+            print(df_concat['DOB'][j])        
+            k3_count +=1
+        elif k == 2:
+            print(k,l,n)
+            df_concat['DOB'][j] = str(df_concat['DOB'][j]).split('/')[1]
+            print(df_concat['DOB'][j])    
+            k2_count +=1 
+        elif k == 1:
+            print(k,l,n)
+            df_concat['DOB'][j] = str(df_concat['DOB'][j]).split('/')[0]
+            print(df_concat['DOB'][j])    
+            all1_count +=1       
+              
+    
+    elif n == max(k,n,l):
+        if n >= 3:        
+            print(k,l,n)
+            df_concat['DOB'][j] = str(df_concat['DOB'][j]).split('-')[n-1]
+            print(df_concat['DOB'][j]) 
+            n3_count +=1
+        elif n == 2:
+            print(k,l,n)
+            df_concat['DOB'][j] = str(df_concat['DOB'][j]).split('-')[1]
+            print(df_concat['DOB'][j]) 
+            n2_count +=1           
+    
+    elif l == max(k,n,l):
+        if l >= 3:
+            print(k,l,n)
+            df_concat['DOB'][j] = str(df_concat['DOB'][j]).split(' ')[(l-1)]
+            print('look at index:%s' %j)  
+            print(df_concat['DOB'][j])          
+            l3_count +=1
+        elif l == 2:
+            print(k,l,n)
+            df_concat['DOB'][j] = str(df_concat['DOB'][j]).split(' ')[1]
+            print(df_concat['DOB'][j])    
+            l2_count +=1
+          
+    elif k == l == n:    
+        print(k,l,n)
+        df_concat['DOB'][j] = str(df_concat['DOB'][j]).split(' ')[0]
+        print(df_concat['DOB'][j])   
+        all1_count +=1 
+    else:
+        '''Maybe there should be an error generated if there are not 2 or 3 elements of the list,
+        this implies a date is in some other format 
+        '''
+        print(' look at index:%s' %j)  
+        print(k,l,n)
+        
+    j +=1
+  
+  
+  
+k_max_d=0
+n_max_d=0
+l_max_d=0
+k3_count_d=0
+k2_count_d=0
+n3_count_d=0
+n2_count_d=0  
+l3_count_d=0
+l2_count_d=0
+all1_count_d=0
+j=0
+m = df_concat.shape[0]
+while j < m:
+    
+        
+    k=len(str(df_concat.iloc[j,3]).split('/'))
+    n=len(str(df_concat.iloc[j,3]).split('-'))
+    l=len(str(df_concat.iloc[j,3]).split(' '))
+    p = max(k,n,l)
+    if k > k_max_d:
+        k_max=k
+
+    if n > n_max_d:
+        n_max=n
+        
+    if l > l_max_d:
+        l_max=l
+ 
+    if j == 115:
+        print(k,l,n)
+    
+    if k == max(k,n,l):
+        if k >= 3:
+            print(k,l,n)
+            df_concat['DOD'][j] = str(df_concat['DOD'][j]).split('/')[k-1]
+            print(df_concat['DOD'][j])        
+            k3_count_d +=1
+        elif k == 2:
+            print(k,l,n)
+            df_concat['DOD'][j] = str(df_concat['DOD'][j]).split('/')[1]
+            print(df_concat['DOD'][j])    
+            k2_count_d +=1 
+        elif k == 1:
+            print(k,l,n)
+            print(df_concat['DOD'][j])
+            df_concat['DOD'][j] = str(df_concat['DOD'][j]).split('/')[0]
+            print(df_concat['DOD'][j])    
+            all1_count_d +=1       
+              
+    
+    elif n == max(k,n,l):
+        if n >= 3:        
+            print(k,l,n)
+            df_concat['DOD'][j] = str(df_concat['DOD'][j]).split('-')[n-1]
+            print(df_concat['DOD'][j]) 
+            n3_count_d +=1
+        elif n == 2:
+            print(k,l,n)
+            df_concat['DOD'][j] = str(df_concat['DOD'][j]).split('-')[1]
+            print(df_concat['DOD'][j]) 
+            n2_count_d +=1           
+    
+    elif l == max(k,n,l):
+        if l >= 3:
+            print(k,l,n)
+            df_concat['DOD'][j] = str(df_concat['DOD'][j]).split(' ')[(l-1)]
+            print('look at index:%s' %j)  
+            print(df_concat['DOD'][j])          
+            l3_count_d +=1
+        elif l == 2:
+            print(k,l,n)
+            df_concat['DOD'][j] = str(df_concat['DOD'][j]).split(' ')[1]
+            print(df_concat['DOD'][j])    
+            l2_count_d +=1
+          
+    elif k == l == n:    
+        print(k,l,n)
+        df_concat['DODB'][j] = str(df_concat['DOD'][j]).split(' ')[0]
+        print(df_concat['DOD'][j])   
+        all1_count_d +=1 
+    else:
+        '''Maybe there should be an error generated if there are not 2 or 3 elements of the list,
+        this implies a date is in some other format 
+        '''
+        print(' look at index:%s' %j)  
+        print(k,l,n)
+        
+    j +=1
+#print(df_concat)
+
 
 j=0
 m = df_concat.shape[0]
 while j < m:
-    k=len(str(df_concat.iloc[j,2]).split('/'))
-    l=len(str(df_concat.iloc[j,2]).split(' '))
-    print(df_concat.iloc[j,2])
-    print(str(df_concat.iloc[j,2]).split('/'))
-    print(str(df_concat.iloc[j,2]).split(' '))
-    if k == 3:
-        df_concat['DOB'][j] = str(df_concat['DOB'][j]).split('/')[2]
-        print(df_concat['DOB'][j])
-        
-    elif k==2:
-        df_concat['DOB'][j] = str(df_concat['DOB'][j]).split('/')[1]
-        print(df_concat['DOB'][j])    
-        
-    elif k==1:    
-        if l == 1:
-            df_concat['DOB'][j] = str(df_concat['DOB'][j]).split(' ')[0]
-            print(df_concat['DOB'][j])   
-            
-        elif l == 2:
-            df_concat['DOB'][j] = str(df_concat['DOB'][j]).split(' ')[1]
-            print('look at index:%s' %j)  
-            print(df_concat['DOB'][j])
-           
-        else:
-            df_concat['DOB'][j] = str(df_concat['DOB'][j]).split(' ')[(l-1)]
-            print('look at index:%s' %j)  
-            print(df_concat['DOB'][j])
-            
-    else:
-        '''Maybe there should be an error generated if there are not 2 or 3 elements of the list,
-        this implies a date is in some other format 
-        '''
-        print(' look at index:%s' %j)  
-        
-        continue
-    j +=1
+    if df_concat.DOB[j] == 'nan':
+        df_concat=df_concat.drop(j)
+    elif df_concat.DOD[j] == 'nan':
+        df_concat=df_concat.drop(j)    
+    elif df_concat.DOD[j] == '000':
+        df_concat=df_concat.drop(j)
+    elif df_concat.DOD[j] == '0000':
+        df_concat=df_concat.drop(j)   
+    elif df_concat.DOD[j] == '':
+        df_concat=df_concat.drop(j) 
+    j +=1    
+
+
+df_concat.info()
+df_concat.astype({'DOB':'int32'})
+df_concat.astype({'DOD':'int32'})
+df_concat.info()
+
     
-j=0
-m = df_concat.shape[0]
-while j < m:
-    k=len(str(df_concat.iloc[j,3]).split('/'))
-    l=len(str(df_concat.iloc[j,3]).split(' '))
-    print(df_concat.iloc[j,3])
-    print(str(df_concat.iloc[j,3]).split('/'))
-    print(str(df_concat.iloc[j,3]).split(' '))
-    if k == 3:
-        df_concat['DOD'][j] = str(df_concat['DOD'][j]).split('/')[2]
-        print(df_concat['DOD'][j])
-        
-    elif k==2:
-        df_concat['DOD'][j] = str(df_concat['DOD'][j]).split('/')[1]
-        print(df_concat['DOD'][j])    
-        
-    elif k==1:    
-        if l == 1:
-            df_concat['DOD'][j] = str(df_concat['DOD'][j]).split(' ')[0]
-            print(df_concat['DOD'][j])   
-            
-        elif l == 2:
-            df_concat['DOD'][j] = str(df_concat['DOD'][j]).split(' ')[1]
-            print('look at index:%s' %j)  
-            print(df_concat['DOD'][j])
-           
-        else:
-            df_concat['DOD'][j] = str(df_concat['DOD'][j]).split(' ')[(l-1)]
-            print('look at index:%s' %j)  
-            print(df_concat['DOD'][j])
-            
-    else:
-        '''Maybe there should be an error generated if there are not 2 or 3 elements of the list,
-        this implies a date is in some other format 
-        '''
-        print(' look at index:%s' %j)  
-        
-        continue
-    j +=1
-#print(df_concat)
-df_concat.isna()
-df_concat_clean=df_concat.dropna()
+df_concat['LifeSpan'] = df_concat['DOD'] - df_concat['DOB']
+#df_concat_clean=df_concat.dropna()
+
+
 print(df_concat.isna())
  
